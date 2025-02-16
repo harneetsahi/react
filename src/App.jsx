90b9618ce142fa5a7d2cf0f05280f899bd79cdb2
Harneet <harneetsahi8@gmail.com>
2025-02-15 22:34:39 -0600

    downgraded react version to set up recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import { useEffect, useState } from "react";
+import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from "recoil";
+
+const count = atom({
+  key: "countState",
+  default: 0,
+});
 
 function App() {
-  return <div>hi there</div>;
+  return (
+    <div>
+      <RecoilRoot>
+        <Value />
+        <Increase />
+        <Decrease />
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Increase() {
+  const setCount = useSetRecoilState(count);
+  return <button onClick={() => setCount((c) => c + 1)}>Increase</button>;
+}
+
+function Decrease() {
+  const setCount = useSetRecoilState(count);
+  return <button onClick={() => setCount((c) => c - 1)}>Decrease</button>;
+}
+
+function Value() {
+  const countVal = useRecoilValue(count);
+  return <div>{countVal}</div>;
 }
 
 export default App;
